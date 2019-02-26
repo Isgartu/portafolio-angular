bef14894a5e512c9fd5d9a3537bd03ceedec0182
isgartu <isgartu@gmail.com>
2019-02-26 17:00:08 +0100

    Fin seccion 4

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,28 @@
+import { InfoPagina } from './../interfaces/info-pagina.interface';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  constructor( private http: HttpClient) {
+
+    // console.log('servicio de infopagina listo');
+
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+
+        this.cargada = true;
+        this.info = resp;
+
+        console.log( resp );
+        console.log( resp['twitter'] );
+      });
+   }
+}
